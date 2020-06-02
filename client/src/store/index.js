6a7a363af62045e718cf40aec78ed90889ff5922
Jessica Stamos <jessica@jessicastamos.com>
2020-06-02 17:28:41 -0500

    Persist state

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import stocks from './modules/stocks';
 
@@ -9,4 +10,12 @@ export default new Vuex.Store({
   modules: {
     stocks,
   },
+  plugins: [
+    createPersistedState({
+      key: 'stocks',
+      paths: [
+        'stocks',
+      ],
+    }),
+  ],
 });
