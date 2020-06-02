b36dd4d1c6995755c9f893e7bd7f6ac88d176255
Jessica Stamos <jessica@jessicastamos.com>
2020-06-02 15:31:38 -0500

    Base for stocks

---
+++ client/src/store/modules/stocks.js
@@ -0,0 +1,15 @@
+const actions = {};
+
+const getters = {};
+
+const mutations = {};
+
+const state = {};
+
+export default {
+  namespaced: true,
+  actions,
+  getters,
+  mutations,
+  state,
+};
