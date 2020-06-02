6cef8563be6314a9c101d30a40492245d8f9c6e3
Jessica Stamos <jessica@jessicastamos.com>
2020-06-02 15:50:47 -0500

    Adjustments for warnings

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'global-require': 'off',
     'linebreak-style': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
